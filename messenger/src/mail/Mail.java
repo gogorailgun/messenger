6a628c57b2042c43c83465a6d57e8e733781d177
kysan1014 <kysan1014@icloud.com>
2020-09-29 15:36:35 +0900

    main frame commit

--- messenger/src/mail/Mail.java
+++ messenger/src/mail/Mail.java
@@ -15,11 +15,11 @@ public class Mail {
 	
 	// 메인 화면 리스트
 	JFrame fr ;
-	JPanel pan1 , pan2, pan3;
-	JButton btn1 , btn2 , btn3;
-	JList list1, list2;
+	JPanel panIdList , panMsgList, panBtn;
+	JButton btnSend , btnShowMsg , btnClose;
+	JList idList, MsgList;
 	JScrollPane scroll1, scroll2;
-	JLabel lbl1, lbl2;
+	JLabel lblIdList, lblMsgList;
 	
 	// UDP 통신을 할 DatagramSocket 만들기
 	public DatagramSocket sSocket, rSocket;
@@ -31,58 +31,63 @@ public class Mail {
 	public void setUI() {
 		fr = new JFrame("*** 메일함 ***");
 		fr.addWindowListener(new WindowAdapter() {
-			public void windowColsing(WindowEvent e) {
+			@Override
+			public void windowClosing(WindowEvent e) {
 				close();
 				System.exit(0);
 			}
+
 		});
 		
 		// 판넬 추가
-		pan1 = new JPanel(new BorderLayout());
-		pan2 = new JPanel(new BorderLayout());
-		pan3 = new JPanel(new GridLayout(1,3));
+		JPanel pan = new JPanel(new GridLayout(1,2));
+		panIdList = new JPanel(new BorderLayout());
+		panMsgList = new JPanel(new BorderLayout());
+		panBtn = new JPanel(new GridLayout(1,3));
 		
-		pan1.setSize(new Dimension(245,230));
-		pan2.setSize(new Dimension(245,230));
-		pan3.setSize(new Dimension(30,500));
+		panIdList.setSize(new Dimension(245,230));
+		panMsgList.setSize(new Dimension(245,230));
+		panBtn.setSize(new Dimension(30,500));
 		
 		// list 추가
-		list1 = new JList();
-		list2 = new JList();
+		idList = new JList();
+		MsgList = new JList();
 		
 		// label 추가
-		lbl1 = new JLabel("회원정보");
-		lbl2 = new JLabel("메일함");
+		lblIdList = new JLabel("회원정보");
+		lblIdList.setHorizontalAlignment(JLabel.CENTER);
+		lblMsgList = new JLabel("메일함");
+		lblMsgList.setHorizontalAlignment(JLabel.CENTER);
 		
-		list1.setSize(new Dimension(240,230));
-		list2.setSize(new Dimension(240,230));
+		idList.setSize(new Dimension(240,230));
+		MsgList.setSize(new Dimension(240,230));
 		
-		pan1.add(list1);
-		pan1.add(lbl1,BorderLayout.NORTH);
-		pan2.add(list2);
-		pan2.add(lbl2,BorderLayout.NORTH);
+		panIdList.add(idList);
+		panIdList.add(lblIdList,BorderLayout.NORTH);
+		panMsgList.add(MsgList);
+		panMsgList.add(lblMsgList,BorderLayout.NORTH);
 		
 		// 버튼 추가
-		btn1 = new JButton("보내기");
-		btn2 = new JButton("보기");
-		btn3 = new JButton("닫기");
+		btnSend = new JButton("보내기");
+		btnShowMsg = new JButton("보기");
+		btnClose = new JButton("닫기");
 		
 		// 버튼 이벤트 추가
-		btn1.addActionListener(new ActionListener() {
+		btnSend.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				SendFrame sFr = new SendFrame(Mail.this);
 			}
 		});
 		
-		btn2.addActionListener(new ActionListener() {
+		btnShowMsg.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				ReceiveFrame rFr = new ReceiveFrame(Mail.this);
 			}
 		});
 		
-		btn3.addActionListener(new ActionListener() {
+		btnClose.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				close();
@@ -90,15 +95,16 @@ public class Mail {
 			}
 		});
 		
-		pan3.add(btn1);
-		pan3.add(btn2);
-		pan3.add(btn3);
+		panBtn.add(btnSend);
+		panBtn.add(btnShowMsg);
+		panBtn.add(btnClose);
 		
 		
 		// frame 설정
-		fr.add(pan1, BorderLayout.WEST);
-		fr.add(pan2, BorderLayout.EAST);
-		fr.add(pan3, BorderLayout.SOUTH);
+		pan.add(panIdList, BorderLayout.WEST);
+		pan.add(panMsgList, BorderLayout.EAST);
+		fr.add(pan, BorderLayout.CENTER);
+		fr.add(panBtn, BorderLayout.SOUTH);
 		
 		fr.setSize(500, 500);
 		fr.setVisible(true);
